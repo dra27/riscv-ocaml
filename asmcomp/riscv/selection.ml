a42166ad003c84bb498de4de714f3d086d8230d5
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2018-10-03 07:15:01 +0000

    Adapt to changes in trunk

--- asmcomp/riscv/selection.ml
+++ asmcomp/riscv/selection.ml
@@ -56,17 +56,16 @@ method! select_operation op args dbg =
 (* Instruction selection for conditionals *)
 
 method! select_condition = function
-  | Cop(Ccmpi cmp, args, _) ->
+    Cop(Ccmpi cmp, args, _) ->
       (Iinttest(Isigned cmp), Ctuple args)
   | Cop(Ccmpa cmp, args, _) ->
       (Iinttest(Iunsigned cmp), Ctuple args)
   | Cop(Ccmpf cmp, args, _) ->
-      (Ifloattest(cmp, false), Ctuple args)
+      (Ifloattest cmp, Ctuple args)
   | Cop(Cand, [arg; Cconst_int 1], _) ->
       (Ioddtest, arg)
   | arg ->
       (Itruetest, arg)
-
 end
 
 let fundecl f = (new selector)#emit_fundecl f
